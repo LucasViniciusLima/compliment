d412d7bc49eac0c558f0e5c73f80539d3381a096
Lucas_Vinícius_Lima_Bezerra <lucaslimavzt@gmail.com>
2021-08-19 17:21:32 -0300

    nav menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule
